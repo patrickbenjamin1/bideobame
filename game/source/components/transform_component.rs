2d775a4d8ec4b4a3cf13873eeb8d3cbc8f5b9d7b
pat <dev@patrickbenjam.in>
2024-11-11 23:26:57 +0000

    remove transform system

--- game/source/components/transform_component.rs
+++ game/source/components/transform_component.rs
@@ -1,5 +1,5 @@
 use crate::core::geometry;
-use glam::{Mat4, Vec3, EulerRot};
+use glam::{EulerRot, Mat4, Vec3};
 
 pub struct TransformComponent {
     pub position: geometry::Vector3,
@@ -14,7 +14,7 @@ impl TransformComponent {
         rotation: geometry::Vector3,
         scale: geometry::Vector3,
     ) -> Self {
-        let model = Mat4::from_translation(Vec3::from_slice(&position)) 
+        let model = Mat4::from_translation(Vec3::from_slice(&position))
             * Mat4::from_euler(EulerRot::XYZ, rotation[0], rotation[1], rotation[2])
             * Mat4::from_scale(Vec3::from_slice(&scale));
 
@@ -35,29 +35,52 @@ impl TransformComponent {
         self.position[0] += translation[0];
         self.position[1] += translation[1];
         self.position[2] += translation[2];
+
+        self.update_model_matrix();
     }
 
     pub fn rotate(&mut self, rotation: geometry::Vector3) {
         self.rotation[0] += rotation[0];
         self.rotation[1] += rotation[1];
         self.rotation[2] += rotation[2];
+
+        self.update_model_matrix();
     }
 
     pub fn scale(&mut self, scale: geometry::Vector3) {
         self.scale[0] *= scale[0];
         self.scale[1] *= scale[1];
         self.scale[2] *= scale[2];
+
+        self.update_model_matrix();
     }
 
     pub fn set_position(&mut self, position: geometry::Vector3) {
         self.position = position;
+
+        self.update_model_matrix();
     }
 
     pub fn set_rotation(&mut self, rotation: geometry::Vector3) {
         self.rotation = rotation;
+
+        self.update_model_matrix();
     }
 
     pub fn set_scale(&mut self, scale: geometry::Vector3) {
         self.scale = scale;
+
+        self.update_model_matrix();
+    }
+
+    pub fn update_model_matrix(&mut self) {
+        self.model_matrix = Mat4::from_translation(Vec3::from_slice(&self.position))
+            * Mat4::from_euler(
+                EulerRot::XYZ,
+                self.rotation[0],
+                self.rotation[1],
+                self.rotation[2],
+            )
+            * Mat4::from_scale(Vec3::from_slice(&self.scale));
     }
 }
--- game/source/systems/mod.rs
+++ game/source/systems/mod.rs
@@ -1,3 +1,2 @@
 pub mod mesh_bufferer_system;
 pub mod mesh_renderer_system;
-pub mod transform_system;
